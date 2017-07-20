99704a8693c01e5e0d5696e8b4b8e0617ead59c0
Merzouk HAMRANI <merzouk.hamrani@gmail.com>
2017-07-20 23:50:16 +0200

    add service registry (eureka)

--- service-messages/src/main/java/hm/fr/MessagesApplication.java
+++ service-messages/src/main/java/hm/fr/MessagesApplication.java
@@ -3,6 +3,7 @@ package hm.fr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MessagesApplication {
 
 	public static void main(String[] args) {
